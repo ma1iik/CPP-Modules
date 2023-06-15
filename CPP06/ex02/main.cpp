bda8033c32f161ef650ec5363b7c3ce60880eb0b
ma1iik <misrailo@student.42nice.fr>
2023-06-15 17:07:03 +0200

    finish

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ma1iik <ma1iik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 19:14:45 by ma1iik            #+#    #+#             */
-/*   Updated: 2023/05/21 09:31:53 by ma1iik           ###   ########.fr       */
+/*   Updated: 2023/06/14 21:42:15 by ma1iik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,21 +51,34 @@ void identify(Base* p) {
 }
 
 void identify(Base& p) {
-	if (dynamic_cast<A*>(&p))
-		std::cout << "The type is: A" << std::endl;
-	else if (dynamic_cast<B*>(&p))
-		std::cout << "The type is: B" << std::endl;
-	else if (dynamic_cast<C*>(&p))
-		std::cout << "The type is: C" << std::endl;
-	else
-        std::cout << "Unknown type" << std::endl;	
+    try {
+       	dynamic_cast<A&>(p);
+    	std::cout << "The type is: A" << std::endl;
+		return;
+    }
+    catch (std::exception &e)  {}
+
+    try {
+        dynamic_cast<B&>(p);
+        std::cout << "The type is: B" << std::endl;
+		return;
+    }
+    catch (std::exception &e) {}
+
+    try {
+        dynamic_cast<C&>(p);
+        std::cout << "The type is: C" << std::endl;
+		return;
+    }
+    catch (std::exception &e)  {}
+	std::cout << "Bad Dynamic cast!" << std::endl; return;
 }
 
 int	main() {
 	std::srand(std::time(0)); // use current time as seed for random generator
-    for (int i = 0; i < 10; ++i) {
-        Base* basePtr = generate();
-        identify(basePtr);
+    for (int i = 0; i < 5; ++i) {
+    	Base* basePtr = generate();
+        //identify(basePtr);
         identify(*basePtr);
         delete basePtr; // remember to delete dynamically allocated memory
     }
